065d13f625ace1608763b516b8876dbb6418bcbe
Billard Martin <martin.l.billard@gmail.com>
2016-01-30 12:58:25 +0100

    Clean the useless elements in the page

--- clean.js
+++ clean.js
@@ -1,2 +1,17 @@
+var elements = new Array();
 
-alert("Test alert");
+// All the elements we want to remove
+elements["right_panel"] = document.getElementById("colonneDroite");
+//elements["translate_toolbar"] = document.getElementById("reseaux_sociaux_toolbar");
+elements["menu"] = document.getElementById("menu");
+elements["header"] = document.getElementById("bandeau");
+elements["social_toolbar"] = document.getElementById("reseaux_sociaux_toolbar_bas");
+elements["comments"] = document.getElementById("commentaire");
+elements["donations"] = document.getElementById("forums_don_toolbar");
+elements["fisrt_footer"] = document.getElementById("bas");
+elements["second_footer"] = document.getElementById("piedpage");
+
+for (var keys in elements) {
+  console.log(elements[keys]);
+  elements[keys].parentNode.removeChild(elements[keys]);
+}
