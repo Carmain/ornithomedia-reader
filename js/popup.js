b458f0391c2db7962971724a21e3db505be239ab
Billard Martin <martin.l.billard@gmail.com>
2016-03-22 22:54:15 +0100

    BUGFIX : We could update the configuration

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,21 @@
+//Localize by replacing __MSG_***__ meta tags
+(function localizeHtmlPage()
+{
+  var objects = document.getElementsByTagName('html');
+  for (var j = 0; j < objects.length; j++) {
+    var obj = objects[j];
+
+    var valStrH = obj.innerHTML.toString();
+    var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1) {
+      return v1 ? chrome.i18n.getMessage(v1) : "";
+    });
+
+    if (valNewH != valStrH) {
+      obj.innerHTML = valNewH;
+    }
+  }
+})();
+
 (function init() {
   var itemsForm = document.querySelectorAll(".config");
   var form = document.getElementById('configuration_form');
@@ -21,22 +39,4 @@
   for(var i = 0; i < itemsForm.length; i++) {
     chrome.storage.sync.get(itemsForm[i].id, checkItem);
   }
-
-  //Localize by replacing __MSG_***__ meta tags
-  (function localizeHtmlPage()
-  {
-    var objects = document.getElementsByTagName('html');
-    for (var j = 0; j < objects.length; j++) {
-      var obj = objects[j];
-
-      var valStrH = obj.innerHTML.toString();
-      var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1) {
-        return v1 ? chrome.i18n.getMessage(v1) : "";
-      });
-
-      if (valNewH != valStrH) {
-        obj.innerHTML = valNewH;
-      }
-    }
-  })();
 })();
